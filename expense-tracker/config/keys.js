5e0e621d1c6b3d4c87525c393ca0c42b390e2a0f
manozchhetri <156447784+manozchhetri@users.noreply.github.com>
2025-08-07 23:30:03 -0400

    Initial commit - setup Express app with Handlebars and MongoDB config

---
+++ expense-tracker/config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  MongoURI: process.env.MONGO_URI,
+  SessionSecret: process.env.SESSION_SECRET,
+  GithubClientID: process.env.GITHUB_CLIENT_ID,
+  GithubClientSecret: process.env.GITHUB_CLIENT_SECRET
+};
